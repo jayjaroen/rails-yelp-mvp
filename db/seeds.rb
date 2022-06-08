104fa7090638cb573dcc17d8c5195ba47129ef02
jiraporn jaroensutiyotin <jay.ji.jaroen@gmail.com>
2022-06-08 23:39:46 +0800

    Finish initial version of rails yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "cleaning up database"
+Restaurant.destroy_all
+puts "database is clean"
+
+10.times do
+  Restaurant.create(
+    name: Faker::Restaurant.name,
+    address: Faker::Address.city,
+    phone_number: Faker::PhoneNumber.phone_number,
+    category: ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+end
